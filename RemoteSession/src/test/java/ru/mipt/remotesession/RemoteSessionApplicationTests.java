a91458367009000e75d8c78733f182e1d908d929
alexansemya <aleks.semyachkin@mail.ru>
2022-04-17 22:14:10 +0300

    added h2 DataBase

--- RemoteSession/src/test/java/ru/mipt/remotesession/RemoteSessionApplicationTests.java
+++ RemoteSession/src/test/java/ru/mipt/remotesession/RemoteSessionApplicationTests.java
@@ -1,8 +1,11 @@
 package ru.mipt.remotesession;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+@EntityScan("ru.mipt.remotesession.models")
 @SpringBootTest
 class RemoteSessionApplicationTests {
 
